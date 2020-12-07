169a834134319a9c10ba0a6b63d2e28db1515ca6
René Camacho <Ren Camacho@LT_RCAMACHO>
2020-12-06 22:43:48 -0500

    Carga inicial del proyecto

---
+++ src/prjprueba/PrjPrueba.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjprueba;
+
+/**
+ *
+ * @author René Camacho
+ */
+public class PrjPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO, carga inicial Git Hub");
+        
+    }
+    
+}
