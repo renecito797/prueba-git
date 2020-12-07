f4bda2318e4fad6f4b86ad457ccc1a13f0a817f8
René Camacho <Ren Camacho@LT_RCAMACHO>
2020-12-06 22:51:41 -0500

    Carga inicial del proyecto

--- src/prjprueba/PrjPrueba.java
+++ src/prjprueba/PrjPrueba.java
@@ -16,7 +16,7 @@ public class PrjPrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HOLA MUNDO, este el el primer cambio en rama develop");
+        System.out.println("HOLA MUNDO Este cambio sube a master");
         
     }
     
